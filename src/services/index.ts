45ab395edecdb6978cc6ac22d6e30fda8d76c378
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-22 15:28:46 +0530

    graphqlRequestClient file created and updated.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,29 +1,29 @@
-// import { GraphQLClient } from 'graphql-request';
+import { GraphQLClient } from 'graphql-request';
 
-// import envConfig from '../../utils/envConfig';
-// import { getTokens } from '../../utils/tokensHelper';
+import envConfig from '../utils/envConfig';
+import { getAccessToken } from '../utils/tokensHelper';
 
-// type API_SERVICE = 'magunaServer';
+type API_SERVICE = 'magunaServer';
 
-// const url = (service: API_SERVICE) => {
-//   switch (service) {
-//     case 'magunaServer':
-//       return envConfig.SERVER_URL;
-//   }
-// };
+const url = (service: API_SERVICE) => {
+  switch (service) {
+    case 'magunaServer':
+      return envConfig.SERVER_URL;
+  }
+};
 
-// const graphqlRequestClient = (
-//   service: API_SERVICE = 'magunaServer',
-// ): GraphQLClient => {
-//   const gqlClient = new GraphQLClient(`${url(service)}/graphql` as string);
+const graphqlRequestClient = (
+  service: API_SERVICE = 'magunaServer',
+): GraphQLClient => {
+  const gqlClient = new GraphQLClient(`${url(service)}/graphql` as string);
 
-//   getTokens().then(data => {
-//     if (data.accessToken) {
-//       gqlClient.setHeader('authorization', `Bearer ${data.accessToken}`);
-//     }
-//   });
+  getAccessToken().then(accessToken => {
+    if (accessToken) {
+      gqlClient.setHeader('authorization', `Bearer ${accessToken}`);
+    }
+  });
 
-//   return gqlClient;
-// };
+  return gqlClient;
+};
 
-// export default graphqlRequestClient;
+export default graphqlRequestClient;
