a18cd438163c71eee608706f5f4df97ba9a75dba
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-05-24 14:28:59 +0530

    ACCESS_TOKEN changed to BEARER_ACCESS_TOKEN

--- src/utils/tokensHelper.ts
+++ src/utils/tokensHelper.ts
@@ -4,27 +4,27 @@ import {
   resetGenericPassword,
 } from 'react-native-keychain';
 
-const ACCESS_TOKEN = 'ACCESS_TOKEN';
+const BEARER_ACCESS_TOKEN = 'BEARER_ACCESS_TOKEN';
 
 export async function getAccessToken() {
   try {
     const credentials = await getGenericPassword();
-    if (credentials && credentials.username === ACCESS_TOKEN) {
+    if (credentials && credentials.username === BEARER_ACCESS_TOKEN) {
       return credentials.password;
     } else {
       return null;
     }
   } catch (error) {
-    console.log(`Could not retrieve token for ${ACCESS_TOKEN}`, error);
+    console.log(`Could not retrieve token for ${BEARER_ACCESS_TOKEN}`, error);
     return null;
   }
 }
 
 export async function setAccessToken(token: string) {
   try {
-    await setGenericPassword(ACCESS_TOKEN, token);
+    await setGenericPassword(BEARER_ACCESS_TOKEN, token);
   } catch (error) {
-    console.log(`Could not store token for ${ACCESS_TOKEN}`, error);
+    console.log(`Could not store token for ${BEARER_ACCESS_TOKEN}`, error);
   }
 }
 
@@ -32,11 +32,11 @@ export async function removeAccessToken() {
   try {
     const credentials = await getGenericPassword();
 
-    if (credentials && credentials.username === ACCESS_TOKEN) {
+    if (credentials && credentials.username === BEARER_ACCESS_TOKEN) {
       await resetGenericPassword();
     } else {
     }
   } catch (error) {
-    console.log(`Could not remove token for ${ACCESS_TOKEN}`, error);
+    console.log(`Could not remove token for ${BEARER_ACCESS_TOKEN}`, error);
   }
 }
